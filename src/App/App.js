b01afc999a49c90ec053c3c759e4caa177e159fb
Eric Weissman <eric.weissman21@gmail.com>
2022-07-29 10:52:17 -0600

    Add styles

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import BookArea from '../BookArea/BookArea.js'
+import { allBooks } from '../list.js'
 
-function App() {
+const App = () => {
+  const [books, setBooks] = useState(allBooks)
+
+  const removeBook = (id) => {
+    const cleanedBooks = books.filter(book => book.id !== id);
+    setBooks(cleanedBooks);
+  }
+
+  const unread = books.filter(book => !book.read);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>bookclub.</h1>
+      <BookArea
+        books={unread}
+        remove={removeBook}
+      />
     </div>
-  );
+  )
 }
 
 export default App;
