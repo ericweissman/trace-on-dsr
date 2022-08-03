897f777bf6d1a572d6ba7e9037e60b3e79440a09
Eric Weissman <eric.weissman21@gmail.com>
2022-08-03 16:25:43 -0600

    Adjust BookArea component

--- src/BookArea/BookArea.js
+++ src/BookArea/BookArea.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import Card from '../Card/Card.js';
+import MiniCard from '../MiniCard/MiniCard.js';
 
-function BookArea({books, remove}) {
+function BookArea({books, library, remove}) {
   const bookCards = books.splice(0, 3).map(book => <Card key={book.id}
     {...book} remove={remove}/>)
+
+  // const libraryCards = library.map(book => <MiniCard key={"00" + book.id} {...book}/> )
+  
+  // const trimRead = library.map(book => {
+  //   return {
+  //     title: book.title,
+  //     author: book.author
+  //   }
+  // })
+  
+  // const showLibrary = () => {
+  //   console.log(trimRead)
+  //   return library.map(book => <MiniCard key={"00" + book.id} {...book}/>)
+  // } 
+  
   return (
-    <section>
-      {bookCards}
-    </section>
+    <div>
+      <section>
+        {bookCards}
+      </section>
+      {/* <button onClick={showLibrary}>Library</button>
+      <section className='library'>
+        {libraryCards}
+      </section> */}
+    </div>
   )
 }
-
-
-//add book button
-//when clicked, a new book card is added to BookArea
-//
 
 export default BookArea;
