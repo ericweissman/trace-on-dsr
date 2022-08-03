f0281fa780ddc0e2148cdf29fc83b4bf8e2d8396
Eric Weissman <eric.weissman21@gmail.com>
2022-08-03 16:27:02 -0600

    Cleanup code comments

--- src/BookArea/BookArea.js
+++ src/BookArea/BookArea.js
@@ -5,30 +5,12 @@ import MiniCard from '../MiniCard/MiniCard.js';
 function BookArea({books, library, remove}) {
   const bookCards = books.splice(0, 3).map(book => <Card key={book.id}
     {...book} remove={remove}/>)
-
-  // const libraryCards = library.map(book => <MiniCard key={"00" + book.id} {...book}/> )
-  
-  // const trimRead = library.map(book => {
-  //   return {
-  //     title: book.title,
-  //     author: book.author
-  //   }
-  // })
-  
-  // const showLibrary = () => {
-  //   console.log(trimRead)
-  //   return library.map(book => <MiniCard key={"00" + book.id} {...book}/>)
-  // } 
   
   return (
     <div>
       <section>
         {bookCards}
       </section>
-      {/* <button onClick={showLibrary}>Library</button>
-      <section className='library'>
-        {libraryCards}
-      </section> */}
     </div>
   )
 }
